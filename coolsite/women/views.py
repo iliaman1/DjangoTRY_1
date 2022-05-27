a21f528bffc3f8eac64bfba0073c7f0eb5b81c3c
ilia <iliaman1993@gmail.com>
2022-05-27 11:31:09 +0300

    Добавил через inclusion_tag('usertags/list_menu.html') отображение меню

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,16 +3,10 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu = [{'title': "О сайте", 'url_name': 'about'},
-        {'title': "Добавить статью", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}]
-
 
 def index(request):
     posts = Women.objects.all()
     context = {'title': 'Главная страница',
-               'menu': menu,
                'posts': posts,
                'cat_selected': 0}
     return render(request, 'women/index.html', context=context)
@@ -23,7 +17,7 @@ def show_post(request, post_id):
 
 
 def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, catid):
@@ -39,7 +33,6 @@ def show_category(request, cat_id):
         raise Http404()
 
     context = {'title': 'Отображение по рубрикам',
-               'menu': menu,
                'posts': posts,
                'cat_selected': cat_id}
     return render(request, 'women/index.html', context=context)

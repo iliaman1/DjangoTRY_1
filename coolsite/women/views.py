5e13314e814d703bc3084970894cffa5380ce348
ilia <iliaman1993@gmail.com>
2022-05-27 14:36:58 +0300

    Делаю миграции. Поменял модельку и удалил старую бд(страшно)

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -13,7 +13,9 @@ def index(request):
 
 
 def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с ид = {post_id}")
+    post = get_object_or_404(Women, pk = post_id)
+    context = {'post': post, 'title': post.title, 'cat_selected': post.cat_id,}
+    return render(request, 'women/post.html', context=context)
 
 
 def about(request):

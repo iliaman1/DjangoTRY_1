bb11945b6777d9f2601d67fe7d31e6d81bc18926
ilia <iliaman1993@gmail.com>
2022-06-06 10:12:11 +0300

    добавил модель коментариев

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -25,6 +25,14 @@ class Women(models.Model):
         ordering = ['time_create', 'title']
 
 
+class Comment(models.Model):
+    email = models.CharField(max_length=50, verbose_name='Email')
+    content = models.TextField(blank=True, verbose_name='Текст коментария')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время публикации')
+    like = models.IntegerField(default=0, verbose_name='Лайки')
+    post = models.ForeignKey('Women', on_delete=models.PROTECT, verbose_name='Пост')
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name='Название категории')
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')

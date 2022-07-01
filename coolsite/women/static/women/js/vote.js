771180d482ab31eae94e2c01df8dab843cfa9363
pupsik <iliaman1993@gmail.com>
2022-07-01 20:48:09 +0300

    написал отдельную функцию чтоб не дублировать код для дизлайка(дизлайк работает)

--- coolsite/women/static/women/js/vote.js
+++ coolsite/women/static/women/js/vote.js
@@ -1,21 +1,26 @@
 $(document).ready(function(){
-  $('#like').click(function(){
+  function vote_button(id_button){
+      console.log('upload button clicked!')
+      var fd = new FormData();
 
-        console.log('upload button clicked!')
-        var fd = new FormData();
-
-        $.ajax({
+       $.ajax({
           data: fd,
           processData: false,
           contentType: false,
           type: 'POST',
-          url: document.location.href + '/like/',
+          url: document.location.href + `/${id_button}/`,
           success: function(data){
-              document.getElementById("like").innerHTML = document.getElementById("like").textContent.replace(/\d+$/, parseInt($('#like').attr('value'), 10)+1);
-              $('#like').val(parseInt($('#like').attr('value'), 10)+1);
+              document.getElementById(`${id_button}`).innerHTML = document.getElementById(`${id_button}`).textContent.replace(/\d+$/, parseInt($(`#${id_button}`).attr('value'), 10)+1);
+              $(`#${id_button}`).val(parseInt($(`#${id_button}`).attr('value'), 10)+1);
               console.log('upload success!')
 
           }
         });
-    });
+  }
+  $('#like').click(function(){
+      vote_button('like')
+  });
+  $('#dislike').click(function(){
+      vote_button('dislike')
+  });
 });

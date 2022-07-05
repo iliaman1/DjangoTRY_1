48b9b84092e0f2724725e3640ec02d9dde636533
ilia <iliaman1993@gmail.com>
2022-07-05 18:55:46 +0300

    Пытался сделать лайки на комменты работает только на первый коммент (((

--- coolsite/women/static/women/js/vote.js
+++ coolsite/women/static/women/js/vote.js
@@ -1,47 +1,53 @@
 $(document).ready(function(){
-  function vote_button(id_button, id_comment){
-      console.log('upload button clicked!')
-      id_comment = id_comment || undefined
-      var fd = new FormData();
+    function vote_post_button(id_button){
+        console.log('upload button clicked!')
+        var fd = new FormData();
 
-      if (id_comment) {
-          $.ajax({
-              data: fd,
-              processData: false,
-              contentType: false,
-              type: 'POST',
-              url: document.location.href + `/${id_button}/`,
-              success: function(data)
-                  {
-                  console.log($('#like').innerHTML)
-                  document.getElementById(`${id_button}`).innerHTML = document.getElementById(`${id_button}`).textContent.replace(/\d+$/, parseInt($(`#${id_button}`).attr('value'), 10)+1);
-                  $(`#${id_button}`).val(parseInt($(`#${id_button}`).attr('value'), 10)+1);
-                  console.log('upload success!')
-                  }
-                });
-      } else {
-
-
-       $.ajax({
-          data: fd,
-          processData: false,
-          contentType: false,
-          type: 'POST',
-          url: document.location.href + `/${id_button}/`,
-          success: function(data)
-              {
-              console.log($('#like').attr('data-likes'))
-              document.getElementById(`${id_button}`).innerHTML = document.getElementById(`${id_button}`).textContent.replace(/\d+$/, parseInt($(`#${id_button}`).attr('value'), 10)+1);
-              $(`#${id_button}`).val(parseInt($(`#${id_button}`).attr('value'), 10)+1);
-              console.log('upload success!')
-              }
+        $.ajax({
+            data: fd,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            url: document.location.href + `/${id_button}/`,
+            success: function(data)
+                {
+                console.log($('#like').attr('data-likes'))
+                console.log($('#comment_like').val())
+                document.getElementById(`${id_button}`).innerHTML = document.getElementById(`${id_button}`).textContent.replace(/\d+$/, parseInt($(`#${id_button}`).attr('value'), 10)+1);
+                $(`#${id_button}`).val(parseInt($(`#${id_button}`).attr('value'), 10)+1);
+                console.log('upload success!')
+                }
         });
-      }
-  }
-  $('#like').click(function(){
-      vote_button('like')
-  });
-  $('#dislike').click(function(){
-      vote_button('dislike')
-  });
+    }
+
+    function vote_comment_button(id_button){
+        console.log('upload button clicked!')
+        var fd = new FormData();
+        id_comment = $(`#${id_button}`).val()
+        $.ajax({
+            data: fd,
+            processData: false,
+            contentType: false,
+            type: 'POST',
+            url: document.location.href + `/${id_button}/` + `${id_comment}`,
+            success: function(data)
+                {
+                 console.log( id_comment = $(`#${id_button}`).val())
+                console.log('upload success!')
+                }
+        });
+    }
+
+    $('#like').click(function(){
+      vote_post_button('like')
+    });
+    $('#dislike').click(function(){
+      vote_post_button('dislike')
+    });
+    $('#comment_like').click(function(){
+      vote_comment_button('comment_like')
+    });
+    $('#comment_dislike').click(function(){
+      vote_comment_button('comment_dislike')
+    });
 });

9538ac51bc1b0f19f154e47fad2a875d0f8b240f
ilia <iliaman1993@gmail.com>
2022-07-11 15:38:47 +0300

    лайки дизлайки комментов постов работают

--- coolsite/women/static/women/js/vote.js
+++ coolsite/women/static/women/js/vote.js
@@ -26,11 +26,26 @@ $(document).ready(function(){
             }
         }
 
-        dislike(post_slug){
-            if(this.process(`/${post_slug}/dislike`) === true){
-                document.getElementById(`dislike`).innerHTML = document.getElementById(`dislike`).textContent.replace(/\d+$/, parseInt($(`#dislike`).attr('value'), 10)-1);
-                $(`#dislike`).val(parseInt($(`#dislike`).attr('value'), 10)-1);
+        dislike(){
+            if(this.process(document.location.href + '/dislike/') === true){
+                document.getElementById(`dislike`).innerHTML = document.getElementById(`dislike`).textContent.replace(/\d+$/, parseInt($(`#dislike`).attr('value'), 10)+1);
+                $(`#dislike`).val(parseInt($(`#dislike`).attr('value'), 10)+1);
                 console.log('upload success!')
+            }
+        }
+    }
+
+
+    class CommentVote extends Vote{
+        like(commen_id){
+            if(this.process(document.location.href + `/comment_like/${commen_id}`) === true){
+
+            }
+        }
+
+        dislike(commen_id){
+            if(this.process(document.location.href + `/comment_dislike/${commen_id}`) === true){
+
             }
         }
     }
@@ -38,7 +53,25 @@ $(document).ready(function(){
 
     let postvote = new PostVote();
     $('#like').click(function(){
+         document.getElementById(`like`).innerHTML = document.getElementById(`like`).textContent.replace(/\d+$/, parseInt($(`#like`).attr('value'), 10)+1);
+         $(`#like`).val(parseInt($(`#like`).attr('value'), 10)+1);
         postvote.like()
     })
+    $('#dislike').click(function(){
+        postvote.dislike()
+    })
 
+    let commentvote = new CommentVote();
+    $('.comment_like').click(function(){
+        let id_comment = this.value
+        console.log(this.attributeName)
+        // let replaced = this.innerHTML.replace(/\d+$/, parseInt(data, 10)+1);
+        // console.log(replaced)
+        commentvote.like(id_comment)
+
+    })
+    $('.comment_dislike').click(function(){
+        let id_comment = this.value
+        commentvote.dislike(id_comment)
+    })
 });
